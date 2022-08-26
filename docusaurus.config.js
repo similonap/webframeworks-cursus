7e74259ea4361e4f06398ab7c68b60f688b439e3
Andie Similon <andie.similon@ap.be>
2022-08-26 10:03:27 +0200

    Removed blog links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,15 +72,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
@@ -100,10 +91,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
