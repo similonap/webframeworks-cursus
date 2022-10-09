fc9a7de9fdfcd9bff4a0e05b20e2377af794e18e
Andie Similon <andie.similon@ap.be>
2022-10-09 22:30:02 +0200

    Add logos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
         title: 'Webframeworks',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/assimilatehead.png',
         },
         items: [
           {
--- src/pages/index.md
+++ src/pages/index.md
@@ -4,3 +4,5 @@ Je dient een basis van TypeScript te hebben om deze cursus te kunnen volgen.
 
 - [React.js](wf-course)
 - [React Native](react-native-course)
+
+<img src="img/assimilatelogo.png"/>
